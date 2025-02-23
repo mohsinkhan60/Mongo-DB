cb4343bc2bd2587813a83dbd0d7fad6c66383204
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-02-23 22:56:29 +0500

    Add form handling and template for adding todos

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,13 @@
 from application import app
 from flask import render_template
+from application.forms import TodoForm
 
 @app.route('/')
 def index():
     return render_template('view_todos.html', title='view_todos Page')    
+
+    
+@app.route('/add_todo')
+def add_todo():
+    form = TodoForm()
+    return render_template('add_todo.html', form=form)    
