5739835979ea756157aa94f19b1b6aa980418198
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-02-27 01:41:56 +0500

    Implement add todo functionality with form handling and database integration

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,10 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecretkey"    # This is a secret key that is used to protect your application against CSRF attacks.
-app.config['MONGO_URI'] = "mongodb+srv://mohsinkhancontact0:<db_password>@cluster0.tohv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+app.config['MONGO_URI'] = "mongodb+srv://mohsinkhancontact0:Mohsin123khan@cluster0.tohv1.mongodb.net/todo_flask?retryWrites=true&w=majority&appName=Cluster0"
 
 # setup the mongoDB connection
-mongo_client = PyMongo(app)
-db = mongo_client.db
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,29 @@
 from application import app
-from flask import render_template
+from flask import render_template, request, redirect, flash
+from application import db
 from application.forms import TodoForm
+from datetime import datetime
 
 @app.route('/')
 def index():
     return render_template('view_todos.html', title='view_todos Page')    
 
-    
-@app.route('/add_todo')
+@app.route('/add_todo', methods=['POST', 'GET'])
 def add_todo():
-    form = TodoForm()
-    return render_template('add_todo.html', form=form)    
+    if request.method == "POST":
+        form = TodoForm(request.form)
+        todo_name = form.name.data
+        todo_description = form.description.data
+        completed = form.completed.data
+
+        db.todo_flask.insert_one({
+            "name": todo_name,
+            "description": todo_description,
+            "completed": completed,
+            "date_completed": datetime.now()
+        })
+        flash('Todo added successfully', 'success')
+        return redirect("/")
+    else:
+        form = TodoForm()
+    return render_template('add_todo.html', form=form)
